e5c1719ab865d1b90cc9d2e6e6290733d24f6a0a
Anish Datta <anishd3139@gmail.com>
2022-12-06 19:11:26 +0530

    trying to fix deployment 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from 'react-router-dom'
 import "./index.css"
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <HashRouter>
+//             <App />
+//         </HashRouter>
+//     </React.StrictMode>
+// );
