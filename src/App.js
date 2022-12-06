fdcfe005a9af6c35ee2c280cc9bcf3a7b07d95e3
Anish Datta <anishd3139@gmail.com>
2022-12-07 00:50:17 +0530

    trying to fix deployment 6

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import App1 from "./components/App1.js"
 // import MenuLink from "./components/MenuLink";
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import App2 from "./components/App2.js";
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={
+        <Router >
+            <Routes basemname={`/${process.env.PUBLIC_URL}`}>
+                <Route exact path="/Onigiri_react" element={
                     <App1/>
                 } />
-                <Route path="/menu" element={
+                <Route exact path="/Onigiri_react/menu" element={
                     <App2/>
                 } />
             </Routes>
