323a9e6e72ab3ab82ff4b81bb6952e474375f00b
Anish Datta <anishd3139@gmail.com>
2022-12-08 00:43:47 +0530

    trying to deploy 6

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import App1 from "./components/App1.js"
 // import MenuLink from "./components/MenuLink";
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import App2 from "./components/App2.js";
 
 const App = () => {
     return (
         <Router >
             <Routes>
-                <Route exact path="/menu" element={
+                <Route exact path="/Onigiri_react/menu" element={
                     <App2/>
                     // <h1>hello</h1>
                 } />
-                <Route exact path="/" element={
+                <Route exact path="/Onigiri_react/" element={
                     <App1/>
                     // <h1>hello there</h1>
                 } />
